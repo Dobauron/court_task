4ab089c040f2c18bfbadce0c2bbb37602a56221c
dobauron <safetysafetystrazak@gmail.com>
2023-03-15 21:12:45 +0100

    add set name and set booking time

---
+++ reservation/court_reservation.py
@@ -0,0 +1,12 @@
+class Reservation:
+    def __init__(self):
+        self.set_name()
+        self.set_booking_time()
+
+    def set_name(self):
+        self.name = input('What\'s your Name?')
+
+    def set_booking_time(self):
+        self.booking_time = input('When would you like to book? {DD.MM.YYYY HH:MM}')
+
+    def check_booking_time_available(self):
