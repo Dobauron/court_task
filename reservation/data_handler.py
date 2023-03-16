96dfb3ae6d0d3c30ca807b31683854c324e60703
dobauron <safetysafetystrazak@gmail.com>
2023-03-16 03:58:37 +0100

    create save reservation in json file

--- reservation/data_handler.py
+++ reservation/data_handler.py
@@ -17,3 +17,17 @@ class DataHandler:
 
     def print_data(self):
         print(self.data)
+
+
+    def save_reservation_json(self, date_key, data):
+        with open("23.03-30.03.json", "r") as f:
+            existing_reservations = json.load(f)
+        if date_key in existing_reservations:
+            existing_reservations[date_key].append(data)
+        else:
+            existing_reservations[date_key] = [data]
+        # Add the new reservation to the existing reservations
+
+        with open("23.03-30.03.json", "w") as f:
+            # Write the updated reservations to the JSON file
+            json.dump(existing_reservations, f, indent=4)
